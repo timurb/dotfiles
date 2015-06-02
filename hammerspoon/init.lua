91055e8fefe970c814ec05c402d6900258020014
Timur Batyrshin <erthad@gmail.com>
2015-06-02 22:52:46 +0300

    replace mjolnir by hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,41 +1,41 @@
-local application = require "mjolnir.application"
-local hotkey = require "mjolnir.hotkey"
-local window = require "mjolnir.window"
-local fnutils = require "mjolnir.fnutils"
-local alert = require "mjolnir.alert"
-local itunes = require "mjolnir.lb.itunes"
+local application = require "hs.application"
+local hotkey = require "hs.hotkey"
+local window = require "hs.window"
+local fnutils = require "hs.fnutils"
+local alert = require "hs.alert"
+local itunes = require "hs.itunes"
 
 hotkey.bind({"cmd", "alt", "ctrl"}, 'space', itunes.displayCurrentTrack)
 
 hotkey.bind({"cmd", "alt"}, 'up', function ()
-  local win = window.focusedwindow()
+  local win = window.focusedWindow()
   local scr = win:screen():frame()
   local f = win:frame()
   f.x = 0
   f.y = 0
   f.w = scr.w
   f.h = scr.h
-  win:setframe(f)
+  win:setFrame(f)
 end)
 
 hotkey.bind({"cmd", "alt"}, "left", function ()
-  local win = window.focusedwindow()
+  local win = window.focusedWindow()
   local scr = win:screen():frame()
   local f = win:frame()
   f.x = 0
   f.y = 0
   f.w = scr.w / 2
   f.h = scr.h
-  win:setframe(f)
+  win:setFrame(f)
 end)
 
 hotkey.bind({"cmd", "alt"}, "right", function ()
-  local win = window.focusedwindow()
+  local win = window.focusedWindow()
   local scr = win:screen():frame()
   local f = win:frame()
   f.x = scr.w / 2
   f.y = 0
   f.w = scr.w / 2
   f.h = scr.h
-  win:setframe(f)
+  win:setFrame(f)
 end)
