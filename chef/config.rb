8df8bcd6607e9d5ea02a0011c224d02077432860
Timur Batyrshin <erthad@gmail.com>
2017-06-29 08:34:39 +0300

    added chefdk params to chef config

--- chef/config.rb
+++ chef/config.rb
@@ -1,9 +1,7 @@
 # See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
-current_dir = File.dirname(__FILE__)
-
 #
-# Define chef params these using ENV vars
+# Define chef params using ENV vars
 # Use of http://direnv.net is recommended to dynamically set these
 #
 %w(CHEF_NODE_NAME CHEF_CLIENT_KEY CHEF_SERVER_URL).each do |var|
@@ -13,6 +11,9 @@ end
 node_name                ENV['CHEF_NODE_NAME']
 client_key               ENV['CHEF_CLIENT_KEY']
 chef_server_url          ENV['CHEF_SERVER_URL']
+
+
+### Set cookbook_path ###
 
 paths = [ ENV['CHEF_COOKBOOKS_PATH'] ]
 
@@ -25,6 +26,13 @@ paths = paths.compact
 raise "No CHEF_COOKBOOK_PATH vars defined" if paths.empty?
 
 cookbook_path            paths
+
+
+### Define author info ###
+
+cookbook_copyright ENV['CHEF_COOKBOOK_AUTHOR']
+cookbook_email ENV['CHEF_COOKBOOK_EMAIL']
+cookbook_license ENV['CHEF_COOKBOOK_LICENSE']
 
 if chefdk && chefdk.generator
   chefdk.generator.email = ENV['CHEF_COOKBOOK_EMAIL']
